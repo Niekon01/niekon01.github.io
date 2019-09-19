289e961a4b02373aa8cd16c63841dbcb90759150
Niekon01 <adarsh90909090@gmail.com>
2019-09-19 20:01:08 +0530

    main.css and testing if php works

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8">
+<link rel="stylesheet" href="/css/main.css">
 <title>Welcome to my Portfolio</title>
 </head>
 <body>
+	<?php
+		$echo 'Hi';
+	?>
 	<h1>Hi!</h1>
 </body>
 </html>
